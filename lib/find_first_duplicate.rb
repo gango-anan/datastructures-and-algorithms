55a40b21e4a8a1839fe333d50caf82cb1c5c577c
Galiwango Ananiya <galiwango101@gmail.com>
2021-05-11 20:22:47 +0300

    Add leetcode challenges folder and one challenge.

--- lib/find_first_duplicate.rb
+++ lib/find_first_duplicate.rb
@@ -1,12 +1,13 @@
+# frozen_string_literal: true
+
 def first_repeated_element(arr)
   return nil if arr.empty?
 
   counter_hash = Hash.new(0)
-  
+
   arr.each do |elem|
     counter_hash[elem] = counter_hash[elem] += 1
     return elem if counter_hash[elem] > 1
-
   end
 
   nil
