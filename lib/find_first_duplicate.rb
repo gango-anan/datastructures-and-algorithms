99663e16ebda4c6e0022b02671e68517f6d35368
Galiwango Ananiya <galiwango101@gmail.com>
2021-05-11 15:41:17 +0300

    Initial commit.

---
+++ lib/find_first_duplicate.rb
@@ -0,0 +1,13 @@
+def first_repeated_element(arr)
+  return nil if arr.empty?
+
+  counter_hash = Hash.new(0)
+  
+  arr.each do |elem|
+    counter_hash[elem] = counter_hash[elem] += 1
+    return elem if counter_hash[elem] > 1
+
+  end
+
+  nil
+end
